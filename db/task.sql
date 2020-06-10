49e2494f5c261c52bb855d545b4eb725cdf78bdc
883781 <65192075+883781@users.noreply.github.com>
2020-06-10 00:48:44 -0500

    Add files via upload

--- db/task.sql
+++ db/task.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.4.11-MariaDB : Database - task
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -24,13 +25,13 @@ CREATE TABLE `users` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `Name` varchar(30) NOT NULL,
   `Description` varchar(100) NOT NULL,
-  `Creation_Date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `Creation_Date` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`Name`,`Description`,`Creation_Date`) values (1,'HELMER','INGENIERO','0000-00-00 00:00:00.000000'),(2,'WILFREDO','HOLA','2020-06-11 19:28:01.000000');
+insert  into `users`(`id`,`Name`,`Description`,`Creation_Date`) values (1,'helmer','ink','0000-00-00 00:00:00.'),(4,'santi arias','ink','0000-00-00 00:00:00.');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
